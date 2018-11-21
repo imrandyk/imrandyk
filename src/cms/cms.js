dcfc298b7b8105df17e87bbb21e63d077ede3304
Randy Keyes <imrandyk@gmail.com>
2018-11-21 17:25:11 -0600

    Adding field to editor

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,3 +7,12 @@ import ProductPagePreview from './preview-templates/ProductPagePreview'
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+CMS.registerEditorComponent({
+  // Internal id of the component
+  id: 'color',
+  // Visible label
+  label: 'Color',
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{ name: 'color', label: 'Color', widget: 'string' }],
+})
