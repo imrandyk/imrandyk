b05e5908746653f9aa5966f7a8b254e63ccb6809
Randy Keyes <imrandyk@gmail.com>
2018-12-14 21:17:10 -0600

    and again

--- functions/verify.js
+++ functions/verify.js
@@ -1,6 +1,11 @@
 exports.handler = (event, context, callback) => {
+  if (event.queryStringParameters.name !== process.env.PAGE_PASS) {
+    callback(null, {
+      statusCode: 403,
+    })
+  }
   callback(null, {
     statusCode: 200,
-    body: process.env.PAGE_PASS,
+    body: 'good',
   })
 }
